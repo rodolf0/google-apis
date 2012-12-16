453e733db07cb57a359f530c691af34ee50a0766
Rodolfo Granata <warlock.cc@gmail.com>
2012-12-16 05:40:57 -0500

    * filter events by date

--- calendar_util.py
+++ calendar_util.py
@@ -4,47 +4,69 @@ import argparse
 import httplib2
 from apitools.auth import getCredentials
 from apiclient.discovery import build
+from datetime import date, datetime, time, timedelta
 import json
 
-def parse_cmdline():
-  "parse commandline options"
-  p = argparse.ArgumentParser(description="Calendar API test")
-  return p.parse_args()
-
-def events(service, calendar):
+def events(service, calendar, date=None):
+  "https://developers.google.com/google-apps/calendar/v3/reference/events/list"
   args = {"maxResults": 1000,
           "fields": ",".join([
             "items/summary",
-            "items/start/date",
+            "items/start",
+            "items/status",
             "items/recurrence",
             "nextPageToken"
           ])}
+  if date:
+    d = datetime.combine(date, time(0, 0, 0))
+    # lower bound for event's end time
+    args["timeMin"] = d.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+    # upper bound for event's start time
+    args["timeMax"] = (d + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+
   while True:
     result = service.events().list(calendarId=calendar,**args).execute()
-    args["pageToken"] = result.get("nextPageToken", "")
-    for i in result["items"]: yield i
-    if not args["pageToken"]: break
+    if "items" not in result:
+      break
+    for i in result["items"]:
+      if i["status"] != "cancelled":
+        yield i
+    if "nextPageToken" in result:
+      args["pageToken"] = result["nextPageToken"]
+    else:
+      break
 
 
-
-def main():
-  args = parse_cmdline()
-
+def list_events(date=None):
   c = getCredentials(".secrets/mysecrets.json", ".secrets/mytokens.dat",
                      "https://www.googleapis.com/auth/calendar")
-
   s = build("calendar", "v3", http=c.authorize(httplib2.Http()))
-
   with open(".secrets/mycalendars.json") as calfile:
     calendars = json.load(calfile)
-
   for cal in calendars.values():
-    for e in events(s, cal):
-      print(e)
+    for e in events(s, cal, date):
+      print("%s %s" % (e["start"].get("date") or e["start"].get("dateTime"), e["summary"]))
 
 
 if __name__ == '__main__':
-  main()
+  def parse_cmdline():
+    "parse commandline options"
+    p = argparse.ArgumentParser(description="Calendar API test")
+    g = p.add_mutually_exclusive_group(required=True)
+    g.add_argument("--all-events", action="store_true")
+    g.add_argument("--today", action="store_true")
+    g.add_argument("--on")
+    return p.parse_args()
+
+  args = parse_cmdline()
+
+  if args.all_events:
+    list_events()
+  elif args.today:
+    list_events(date.today())
+  elif args.on:
+    list_events(datetime.strptime(args.on, '%Y-%m-%d').date())
+
 
 
 # vim: set sw=2 sts=2 : #
